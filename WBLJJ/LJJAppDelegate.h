e915ac1690afa668a31799739ae93ff2e9ef1f6d
Jun <lljwdbklb@163.com>
2013-12-22 11:13:16 +0800

    Initial Commit

---
+++ WBLJJ/LJJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LJJAppDelegate.h
+//  WBLJJ
+//
+//  Created by Jun on 13-12-22.
+//  Copyright (c) 2013年 Jun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LJJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WBLJJ/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WBLJJ
+//
+//  Created by Jun on 13-12-22.
+//  Copyright (c) 2013年 Jun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LJJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LJJAppDelegate class]));
+    }
+}
